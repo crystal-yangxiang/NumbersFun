9123471979294b5c4b69864ea590a5ffdd41a82c
Yang Xiang <love_crystal0126@hotmail.com>
2022-08-11 19:07:21 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NumbersFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
