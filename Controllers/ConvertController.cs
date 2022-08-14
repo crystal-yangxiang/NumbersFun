82d11c08dba51003f752780128657df55a9d7890
Yang Xiang <love_crystal0126@hotmail.com>
2022-08-14 12:48:22 +1000

    add background image and font size and height

--- Controllers/ConvertController.cs
+++ Controllers/ConvertController.cs
@@ -18,16 +18,13 @@ namespace NumbersFun.Controllers
         [HttpPost]
         public IActionResult ConvertNumbers (string numberToConvert)
         {
-            double number = Convert.ToInt32(numberToConvert);
-            //double _tens = Convert.ToInt32(numberToConvert);
-            string word = "zero";
+            double ones = Convert.ToInt32(numberToConvert);
+            double tens = Convert.ToInt32(numberToConvert);
+            string word = "ZERO";
 
 
-            switch (number)
+            switch (ones)
             {
-                //case 0:
-                //    word = "Zero";
-                //    break;
                 case 1:
                     word = "ONE";
                     break;
@@ -57,72 +54,77 @@ namespace NumbersFun.Controllers
                     break;
             }
 
-            //switch (_tens)
-            //{
-            //    case 10:
-            //        word = "TEN";
-            //        break;
-            //    case 11:
-            //        word = "ELEVEN";
-            //        break;
-            //    case 12:
-            //        word = "TWELVE";
-            //        break;
-            //    case 13:
-            //        word = "THIRTEEN";
-            //        break;
-            //    case 14:
-            //        word = "FOURTEEN";
-            //        break;
-            //    case 15:
-            //        word = "FIFTEEN";
-            //        break;
-            //    case 16:
-            //        word = "SIXTEEN";
-            //        break;
-            //    case 17:
-            //        word = "SEVENTEEN";
-            //        break;
-            //    case 18:
-            //        word = "EIGHTEEN";
-            //        break;
-            //    case 19:
-            //        word = "NINETEEN";
-            //        break;
-            //    case 20:
-            //        word = "TWENTY";
-            //        break;
-            //    case 30:
-            //        word = "THIRTY";
-            //        break;
-            //    case 40:
-            //        word = "FOURTY";
-            //        break;
-            //    case 50:
-            //        word = "FIFTY";
-            //        break;
-            //    case 60:
-            //        word = "SIXTY";
-            //        break;
-            //    case 70:
-            //        word = "SEVENTY";
-            //        break;
-            //    case 80:
-            //        word = "EIGHTY";
-            //        break;
-            //    case 90:
-            //        word = "NINETY";
-            //        break;
-            //}
+
+            switch (tens)
+            {
+                case 10:
+                    word = "TEN";
+                    break;
+                case 11:
+                    word = "ELEVEN";
+                    break;
+                case 12:
+                    word = "TWELVE";
+                    break;
+                case 13:
+                    word = "THIRTEEN";
+                    break;
+                case 14:
+                    word = "FOURTEEN";
+                    break;
+                case 15:
+                    word = "FIFTEEN";
+                    break;
+                case 16:
+                    word = "SIXTEEN";
+                    break;
+                case 17:
+                    word = "SEVENTEEN";
+                    break;
+                case 18:
+                    word = "EIGHTEEN";
+                    break;
+                case 19:
+                    word = "NINETEEN";
+                    break;
+                case 20:
+                    word = "TWENTY";
+                    break;
+                case 30:
+                    word = "THIRTY";
+                    break;
+                case 40:
+                    word = "FOURTY";
+                    break;
+                case 50:
+                    word = "FIFTY";
+                    break;
+                case 60:
+                    word = "SIXTY";
+                    break;
+                case 70:
+                    word = "SEVENTY";
+                    break;
+                case 80:
+                    word = "EIGHTY";
+                    break;
+                case 90:
+                    word = "NINETY";
+                    break;
+                //default:
+                //    if (tens > 0)
+                //    {
+                //        word = tens(numberToConvert.Substring(0, 1) + "0") + " " + ones(numberToConvert.Substring(1));
+                //    }
+                //    break;
+            }
 
 
-            ViewBag.number = number;
-           // ViewBag.numTens = tens;
 
-            ViewBag.word = word; 
+            ViewBag.number = ones;
+            ViewBag.numTens = tens;
 
-
-           
+            ViewBag.word = word;
 
 
             return View("Convert", word);
