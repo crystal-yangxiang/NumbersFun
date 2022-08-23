ef9689c1362920a9aa08bd8345a3d0f5d0430d26
Yang Xiang <love_crystal0126@hotmail.com>
2022-08-23 15:16:18 +1000

    ones and tens

--- Controllers/ConvertController.cs
+++ Controllers/ConvertController.cs
@@ -62,16 +62,79 @@ namespace NumbersFun.Controllers
             }
 
             String Tens(String numberToConvert)
-            {
-                double num = Convert.ToInt32(numberToConvert);
-                string word = null;
+            {             
 
+                switch (num)
+                {
+                    case 10:
+                        word = "TEN";
+                        break;
+                    case 11:
+                        word = "ELEVEN";
+                        break;
+                    case 12:
+                        word = "TWELVE";
+                        break;
+                    case 13:
+                        word = "THIRTEEN";
+                        break;
+                    case 14:
+                        word = "FOURTEEN";
+                        break;
+                    case 15:
+                        word = "FIFTEEN";
+                        break;
+                    case 16:
+                        word = "SIXTEEN";
+                        break;
+                    case 17:
+                        word = "SEVENTEEN";
+                        break;
+                    case 18:
+                        word = "EIGHTEEN";
+                        break;
+                    case 19:
+                        word = "NINETEEN";
+                        break;
+                    case 20:
+                        word = "TWENTY";
+                        break;
+                    case 30:
+                        word = "THIRTY";
+                        break;
+                    case 40:
+                        word = "FOURTY";
+                        break;
+                    case 50:
+                        word = "FIFTY";
+                        break;
+                    case 60:
+                        word = "SIXTY";
+                        break;
+                    case 70:
+                        word = "SEVENTY";
+                        break;
+                    case 80:
+                        word = "EIGHTY";
+                        break;
+                    case 90:
+                        word = "NINETY";
+                        break;
+                    default:
+
+
+                        word = Tens(numberToConvert.Substring(0, 1) + "0") + Ones(numberToConvert.Substring(1, 1));
+
+                        break;
+
+                }
                 return word;
             }
 
 
             ViewBag.number = num;
             ViewBag.word = Ones(numberToConvert);
+            ViewBag.word = Tens(numberToConvert);
             return View("Convert", word);
         }
     }
